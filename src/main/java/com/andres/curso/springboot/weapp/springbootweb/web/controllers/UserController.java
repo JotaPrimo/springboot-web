d138012814082c788c7aa3e2e7139e2045280fb2
Jota Santos <jailsonsantosdpge@outlook.com>
2024-01-24 17:45:23 -0400

    13. Pasar datos a la vista

--- src/main/java/com/andres/curso/springboot/weapp/springbootweb/web/controllers/UserController.java
+++ src/main/java/com/andres/curso/springboot/weapp/springbootweb/web/controllers/UserController.java
@@ -1,19 +1,30 @@
 package com.andres.curso.springboot.weapp.springbootweb.web.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 
 @Controller // para espeficicar que é um controlador, é também um componente
 public class UserController {
     // processa request e retorna resposta
 
+    public String[] nomes = {"Jota", "Jessica", "Caleb", "Jughead", "Judy"};
+
 
     @GetMapping("/details")
-    public String details() {
+    public String details(Model model) {
         // as views devem exisir em templates
         // devo retornar o nome da view, não precisa da extensão
+
+        // passando parametros para a view
+        model.addAttribute("titulo", "View details");
+        model.addAttribute("nome", "Jota Santos");
+        model.addAttribute("message", "Bem vindo");
+        model.addAttribute("lista", nomes);
         return "details";
     }
 
