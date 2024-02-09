1d9cb5d17e83865488579c8f03381d05efa123ab
Jota Santos <jailsonsantosdpge@outlook.com>
2024-02-09 17:25:41 -0400

    24. Directiva each (foreach) de Thymeleaf

--- src/main/java/com/andres/curso/springboot/weapp/springbootweb/services/UserService.java
+++ src/main/java/com/andres/curso/springboot/weapp/springbootweb/services/UserService.java
@@ -8,19 +8,16 @@ import java.util.List;
 
 public class UserService {
     public static List<User> getList() {
-        User user = new User("Jota", "Santos");
+        User user = new User("Jota", "Santos", "JotaSantos@gmail.com");
         User user1 = new User("Jessica", "Primo");
         User user2 = new User("Caleb", "Romeo");
-        User user3 = new User("Jughead", "Jones");
-
-//        List<User> list = new ArrayList<>();
-//        list.add(user);
-//        list.add(user1);
-//        list.add(user2);
-//        list.add(user3);
+        User user3 = new User("Jughead", "Jones", "jughead@gmail.com");
+        User user4 = new User("Judy Rua", "Bolinha");
+        User user5 = new User("Joao Emanuel", "Santos Primo");
+        User user6 = new User("Regina Helena", "Regina Helena", "ReginaHelena@gmail.com");
 
         // esta classe é um helper
-        List<User> list = Arrays.asList(user, user1, user2, user3);
+        List<User> list = Arrays.asList(user, user1, user2, user3, user4, user5, user6);
 
         return list;
     }
