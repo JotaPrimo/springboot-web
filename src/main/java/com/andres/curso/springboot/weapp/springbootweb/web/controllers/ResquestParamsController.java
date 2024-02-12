1aeccc9afdcc3b40227f093f1c72b22fe4cc7164
JotaPrimo <gestald118@gmail.com>
2024-02-12 09:57:47 -0400

    Obtener varios parámetros de la URL con @RequestParam

--- src/main/java/com/andres/curso/springboot/weapp/springbootweb/web/controllers/ResquestParamsController.java
+++ src/main/java/com/andres/curso/springboot/weapp/springbootweb/web/controllers/ResquestParamsController.java
@@ -2,6 +2,8 @@ package com.andres.curso.springboot.weapp.springbootweb.web.controllers;
 
 import com.andres.curso.springboot.weapp.springbootweb.config.ApiPath;
 import com.andres.curso.springboot.weapp.springbootweb.models.dto.ParamDto;
+import com.andres.curso.springboot.weapp.springbootweb.models.dto.ParamMixDto;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -15,6 +17,21 @@ public class ResquestParamsController {
     public ParamDto foo(@RequestParam(required = false, defaultValue = "Obrigado Senhor meu Deus") String message) {
         ParamDto paramDto = new ParamDto();
         paramDto.setMessage(message);
+        return paramDto;
+    }
+
+    @GetMapping
+    public ParamDto bar(@RequestParam String text, @RequestParam Integer code) {
+        ParamDto paramDto = new ParamDto();
+        paramDto.setMessage(text);
+        return paramDto;
+    }
+
+    @GetMapping("/request")
+    public ParamMixDto request(HttpServletRequest request) {
+        ParamMixDto paramDto = new ParamMixDto();
+        paramDto.setMessage(request.getParameter("text"));
+        paramDto.setCode(Integer.parseInt(request.getParameter("code")));
         return paramDto;
     }
 }
